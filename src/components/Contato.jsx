8b84c886e8174194167056473c9d2efeffe853bd
gianluca-gazzarini <gianluca.gazzarini@gmail.com>
2023-04-24 17:29:24 -0300

    [REFACTOR] [CARD] Formulário home

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -19,29 +19,26 @@ const Contato = () => {
         <h1 className='title-contact'>ENTRE EM CONTATO</h1>
         <div>
           <div className='contact-small contact-left'>
-            <h1 className='contact-text'>Nome</h1>
-          </div>
+          <label htmlFor='nome' className='contact-text'>Nome</label>
+            <input type='text' id='nome' name='nome' required/>
+        </div>
           <div className='contact-small contact-right'>
-            <h1 className='contact-text'>Celular</h1>
+          <label htmlFor='celular' className='contact-text'>Celular</label>
+            <input type='tel' id='celular' name='celular' pattern='[0-9]{2}[0-9]{5}[0-9]{4}' />
           </div>
           <div className='contact-large'>
-            <h1 className='contact-text'>Email</h1>
+          <label htmlFor='email' className='contact-text'>Email</label>
+            <input type='email' id='email' name='email' required/>
           </div>
           <div className='contact-big'>
-            <h1 className='contact-text'>Mensagem</h1>
+          <label htmlFor='mensagem' className='contact-text'>Mensagem</label>
+            <textarea id='mensagem' name='mensagem' rows='9' cols='33'></textarea>
           </div>
           <div className='contact-button'>
             <h1 className='contact-button-text'>Enviar</h1>
           </div>
         </div>
-        <h1 className='whatsapp-text'>Ou nos chame pelo whatsapp!</h1>
-        <div className='whatsapp'>
-          <a href="https://wa.me/5513996237281?text=Olá!%20Desejo%20saber%20mais%20sobre%20os%20serviços%20da%20Littera!">
-            <img className='whatsapp' src={whatsappGold}/>
-          </a>
-        </div>
-          
-      </div>
+       </div>
     )
   } else {
     return(
@@ -52,33 +49,29 @@ const Contato = () => {
             <br/>
             <br/>
             <div className='contact-small-web contact-left'>
-              <h1 className='contact-text'>Nome</h1>
+            <label htmlFor='nome' className='contact-text'>Nome</label>
+            <input type='text' id='nome' name='nome' required/>
             </div>
             <div className='contact-small-web contact-right'>
-              <h1 className='contact-text'>Celular</h1>
+            <label htmlFor='celular' className='contact-text'>Celular</label>
+            <input type='tel' id='celular' name='celular' pattern='[0-9]{2}[0-9]{5}[0-9]{4}' />
             </div>
             <br/>
             <br/>
             <br/>
             <div className='contact-large-web'>
-              <h1 className='contact-text'>Email</h1>
+            <label htmlFor='email' className='contact-text'>Email</label>
+            <input type='email' id='email' name='email' required/>
             </div>
             <div className='contact-big'>
-              <h1 className='contact-text'>Mensagem</h1>
+            <label htmlFor='mensagem' className='contact-text'>Mensagem</label>
+            <textarea id='mensagem' name='mensagem' rows='7' cols='54'></textarea>
             </div>
             <a>
               <div className='contact-button'>
                 <h1 className='contact-button-text'>Enviar</h1>
               </div>
             </a>
-          </div>
-          <div className='container-wpp'>
-            <h1 className='whatsapp-text-web'>Ou nos chame pelo whatsapp!</h1>
-            <div className='whatsapp'>
-              <a href="https://wa.me/5513996237281?text=Olá!%20Desejo%20saber%20mais%20sobre%20os%20serviços%20da%20Littera!">
-                <img className='whatsapp' src={whatsappGold}/>
-              </a>
-            </div>
           </div>
         </div>          
       </div>
