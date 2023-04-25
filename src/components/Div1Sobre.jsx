0449b6e03d02a07dde752358eebc7680bfdae843
gianluca-gazzarini <gianluca.gazzarini@gmail.com>
2023-04-25 16:06:44 -0300

    [NEW] [CARD] Sobre nós

--- src/components/Div1Sobre.jsx
+++ src/components/Div1Sobre.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+
 import { isMobile } from 'react-device-detect'
+
+import styles from '../style'
 
 const Div1Sobre = () => {
     if(isMobile) {
         return (
             <div>Div1Sobre</div>
+            
           )
     } else {
         return (
-            <div>Div1Sobre</div>
+            
+        <section id="sobre" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+          <div className='bg-div-home-web' >
+            <h1 className='title-web'>CONHEÇA NOSSA</h1>
+            <h1 className='title-sobre-nos2'>EQUIPE!</h1>
+            
+          </div>
+        </section>
           )
     }
 
--- src/components/Proposito.jsx
+++ src/components/Proposito.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+
 import { isMobile } from 'react-device-detect'
+import styles from '../style'
+import { useEffect } from 'react'
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 const Proposito = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 2000});
+      })
+      
     if(isMobile) {
         return (
             <div>Div1Sobre</div>
           )
     } else {
         return (
-            <div>Div1Sobre</div>
+            <section id="proposito" className={`flex flex-col ${styles.paddingY}`}>
+        <div className='bg-div3-sites-web' >
+          <h1 className='title-div3-web'>CONHEÇA NOSSOS</h1>
+          <h1 className='title-proposito'>PROPÓSITOS</h1>
+          <div data-aos="fade-left">
+            <img className='image1-proposito' src="https://images.unsplash.com/photo-1503437313881-503a91226402?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"></img>
+            <div className='card-proposito-web'>
+              <div className='textbox-proposito-web'>
+                 <p className='text-proposito-web'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam autem cupiditate natus enim assumenda et veritatis esse, non distinctio! Reprehenderit quo quod vero nulla impedit magni optio tempore perferendis.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+          </section>
           )
     }
 }
