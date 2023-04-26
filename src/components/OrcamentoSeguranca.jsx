4c2d6239a292bc5994692db34b3b04916a8272a9
leozinho <79992686+leopompeu@users.noreply.github.com>
2023-04-26 19:48:57 -0300

    [REFACTOR] Correção simples

--- src/components/OrcamentoSeguranca.jsx
+++ src/components/OrcamentoSeguranca.jsx
@@ -8,6 +8,7 @@ import { useEffect } from 'react'
 import Aos from 'aos'
 import "aos/dist/aos.css"
 import { useState } from 'react'
+import ModalContato from './ModalContato'
 
 
 const OrcamentoSeguranca = () => {
